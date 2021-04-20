acf51ad5c108a869edc39e6f78aca402a357531c
dzakki <dzakkiaz7@gmail.com>
2021-04-20 08:38:42 +0800

    add column auto number

--- src/navigation/vertical/index.js
+++ src/navigation/vertical/index.js
@@ -1,32 +1,35 @@
 export default [
-  {
-    title: 'Antrian Verifikasi',
-    route: 'antrian-verifikasi',
-    icon: 'HomeIcon',
-  },
+
   {
     title: 'Dashboard',
     route: 'dashboard',
     icon: 'HomeIcon',
   },
   {
-    title: 'Antrian TTV',
-    route: 'antrian-ttv',
-    icon: 'HomeIcon',
-  },
-  {
-    title: 'Antrian Poliklinik',
-    route: 'antrian-poliklinik',
-    icon: 'HomeIcon',
-  },
-  {
-    title: 'List Kehadiran',
-    route: 'list-kehadiran',
-    icon: 'HomeIcon',
-  },
-  {
-    title: 'Logout',
-    route: 'logout',
-    icon: 'HomeIcon',
-  },
+    title: 'Rawat jalan',
+    icon: 'MoreHorizontalIcon',
+    children: [
+      {
+            title: 'Antrian Verifikasi',
+            route: 'antrian-verifikasi',
+            icon: 'HomeIcon',
+          },
+          {
+            title: 'Antrian TTV',
+            route: 'antrian-ttv',
+            icon: 'HomeIcon',
+          },
+          {
+        
+            title: 'Antrian Poliklinik',
+            route: 'antrian-poliklinik',
+            icon: 'HomeIcon',
+          },
+          {
+            title: 'List Kehadiran',
+            route: 'list-kehadiran',
+            icon: 'HomeIcon',
+          },
+    ]
+  }
 ]
