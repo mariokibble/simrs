778ec50dbd9e31ae0217e0fd8771ca37927f3386
maulana_as <maulanaadly05@gmail.com>
2021-04-30 03:11:49 +0700

    [Add] modal and [Change] fetchApi, erase id params

--- src/components/TableAntrianLabPending/TableAntrianLabPending.vue
+++ src/components/TableAntrianLabPending/TableAntrianLabPending.vue
@@ -118,7 +118,10 @@
               v-ripple.400="'rgba(40, 199, 111, 0.15)'"
               variant="flat-success"
               class="btn-icon"
-              @click="$emit('detailIsianLab', { id: props.row.id })"
+              @click="$emit('detailIsianLab', {
+                id: props.row.id,
+                kodeAntrian: props.row.pemeriksaan.kode_antrian,
+                status: props.row.status, })"
             >
               <feather-icon icon="EditIcon" />
             </b-button>
